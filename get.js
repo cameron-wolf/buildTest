cb58d4f162b09713ca9334125ab7b746236956b8
Cameron.Wolf <cameron.wolf@fcc.gov>
2016-11-08 08:30:17 -0500

    V2

--- get.js
+++ get.js
@@ -1,10 +1,10 @@
 function getHi(req,res){
-	console.log('Hello v1');
+	console.log('Hello v2');
 	res.sendStatus(200);
 };
 
 function getBye(req,res){
-	console.log('Goodbye v1');
+	console.log('Goodbye v2');
 	res.sendStatus(200);
 }
 
